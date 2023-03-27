170f4b01dd1851bad7eeafee22f9978803727d08
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2023-03-27 23:00:37 +1100

    Support chords, pauses, repeated notes

--- gp2tex.py
+++ gp2tex.py
@@ -24,9 +24,9 @@ def find_bass_track(song: gp.Song) -> gp.Track | None:
 def track_to_alphatex(track: gp.Track) -> str:
     header = []
     if hasattr(track.song, "title"):
-        header.append(f'\\title "{track.song.title} ({track.name})"')
+        header.append(f'\\title "{track.song.title}"')
     if hasattr(track.song, "subtitle"):
-        header.append(f'\\subtitle "{track.song.subtitle}"')
+        header.append(f'\\subtitle "{track.name}"')
     if hasattr(track.song, "tempo"):
         header.append(f"\\tempo {track.song.tempo}")
     header.append(".")
@@ -40,64 +40,120 @@ def track_to_alphatex(track: gp.Track) -> str:
             curduration = beats[0].duration
             mstr += f":{curduration.value} "
         for beat in beats:
-            note = beat.notes[0]
-            mstr += f"{note.value}.{note.string}"
-            effects = []
-            if note.beat.duration.isDotted:
-                effects.append("d")
-            if note.effect.vibrato:
-                effects.append("v")
-            if note.effect.hammer:
-                effects.append("h")
-            if note.effect.trill is not None:
-                effects.append("tr")
-            if note.effect.palmMute:
-                effects.append("pm")
-            if note.effect.harmonic is not None:
-                if isinstance(note.effect.harmonic, gp.NaturalHarmonic):
-                    effects.append("nh")
-                elif isinstance(note.effect.harmonic, gp.ArtificialHarmonic):
-                    effects.append("ah")
-                elif isinstance(note.effect.harmonic, gp.TappedHarmonic):
-                    effects.append("th")
-                elif isinstance(note.effect.harmonic, gp.PinchHarmonic):
-                    effects.append("ph")
-                elif isinstance(note.effect.harmonic, gp.SemiHarmonic):
-                    effects.append("sh")
-            if note.effect.isBend:
-                effects.append("be")
-                effects.append(
-                    "("
-                    + " ".join(
-                        f"{p.position} {p.value}" for p in note.effect.bend.points
+            # TODO: handle pauses
+            if not beat.notes:
+                bstr = "r"
+                if beat.duration != curduration:
+                    bstr += f".{beat.duration.value}"
+                mstr += bstr + " "
+                continue
+            bstr = ""
+            nstrs = []
+            bdur = None
+            for note in beat.notes:
+                nstrs.append(f"{note.value}.{note.string}")
+                ef = []
+                if note.beat.duration.isDotted:
+                    ef.append("d")
+                if note.effect.vibrato:
+                    ef.append("v")
+                if note.effect.hammer:
+                    ef.append("h")
+                if note.effect.trill is not None:
+                    ef.append("tr")
+                if note.effect.palmMute:
+                    ef.append("pm")
+                if note.effect.harmonic is not None:
+                    if isinstance(note.effect.harmonic, gp.NaturalHarmonic):
+                        ef.append("nh")
+                    elif isinstance(note.effect.harmonic, gp.ArtificialHarmonic):
+                        ef.append("ah")
+                    elif isinstance(note.effect.harmonic, gp.TappedHarmonic):
+                        ef.append("th")
+                    elif isinstance(note.effect.harmonic, gp.PinchHarmonic):
+                        ef.append("ph")
+                    elif isinstance(note.effect.harmonic, gp.SemiHarmonic):
+                        ef.append("sh")
+                if note.effect.isBend:
+                    ef.append("be")
+                    ef.append(
+                        "("
+                        + " ".join(
+                            f"{p.position} {p.value}" for p in note.effect.bend.points
+                        )
+                        + ")"
                     )
-                    + ")"
-                )
-            if note.effect.staccato:
-                effects.append("st")
-            if note.effect.letRing:
-                effects.append("lr")
-            if note.effect.ghostNote:
-                effects.append("g")
-            if note.beat.effect.fadeIn:
-                effects.append("f")
-            if note.beat.duration.tuplet.enters != 1:
-                effects.append(f"tu {note.beat.duration.tuplet.enters}")
-            if effects:
-                mstr += "{" + " ".join(effects) + "}"
-            if note.beat.duration != curduration:
-                mstr += f".{note.beat.duration.value}"
-            mstr += " "
+                if note.effect.staccato:
+                    ef.append("st")
+                if note.effect.letRing:
+                    ef.append("lr")
+                if note.effect.ghostNote:
+                    ef.append("g")
+                if beat.effect.fadeIn:
+                    ef.append("f")
+                if beat.duration.tuplet.enters != 1:
+                    ef.append(f"tu {beat.duration.tuplet.enters}")
+                # Finished parsing effects
+                if ef:
+                    nstrs[-1] += "{" + " ".join(ef) + "}"
+                if beat.duration != curduration:
+                    bdur = beat.duration.value
+            if len(nstrs) > 1:
+                bstr += "(" + " ".join(nstrs) + ")"
+            elif len(nstrs) == 1:
+                bstr = nstrs[0]
+            if bdur is not None:
+                bstr += f".{bdur}"
+            mstr += bstr + " "
         if mstr := mstr.strip():
             mstrs.append(mstr)
     return "\n".join(header) + "\n" + " | \n".join(mstrs)
 
 
+def _replace_spaces(s):
+    """
+    Replace spaces for easier parsing:
+    - keep spaces separating beats,
+    - replace spaces separating notes within a beat with ;
+    - replace spaces separating note effects with _
+    - replace spaces separating details of a note effect with -
+    - remove parentheses around note effects
+    """
+    new_string = ""
+    braces = False
+    inner_parens = False
+    outer_parens = False
+    for char in s:
+        if char == "{":
+            braces = True
+        elif char == "}":
+            braces = False
+        elif char == "(":
+            if braces:
+                inner_parens = True
+                char = ""
+            else:
+                outer_parens = True
+        elif char == ")":
+            if braces:
+                inner_parens = False
+                char = ""
+            else:
+                outer_parens = False
+        elif char == " ":
+            if braces:
+                char = "_"
+                if inner_parens:
+                    char = "-"
+            elif outer_parens:
+                char = ";"
+        new_string += char
+    return new_string
+
+
 def alphatex_to_song(tex: str) -> gp.Song:
-    lines = (l.strip() for l in tex.splitlines())
-
     song = gp.Song()
-
+    lines = (l.strip() for l in tex.splitlines())
     for line in itertools.takewhile(lambda l: l != ".", lines):
         if line.startswith("\\"):
             line = line.lstrip("\\")
@@ -123,103 +179,146 @@ def alphatex_to_song(tex: str) -> gp.Song:
     track = song.tracks[0]
     curduration = gp.Duration(gp.Duration.quarter)
     for mstr in " ".join(lines).split(" | "):
-        # fix up so we can just split beats by space.
-        # replace space inside parens with dash:
-        mstr = re.sub(
-            r"\((.*?)\)", lambda match: match.group(1).replace(" ", "-"), mstr
-        )
-        # replace other spaces inside braces with underscore:
-        mstr = re.sub(r"{(.*?)}", lambda match: match.group(0).replace(" ", "_"), mstr)
+        if not (mstr := mstr.strip()):
+            continue
+        # Fix up so we can just split beats by space
+        mstr = _replace_spaces(mstr)
         measure = track.measures[-1]
-        tokens = mstr.split(" ")
-        for token in tokens:
-            if token.startswith(":"):
-                curduration = gp.Duration(int(token[1:]))
+        for bstr in mstr.split(" "):
+            if bstr.startswith(":"):
+                curduration = gp.Duration(int(bstr[1:]))
+                continue
+            elif bstr.startswith("r"):
+                beat = gp.Beat(measure.voices, duration=curduration)
+                measure.voices[0].beats.append(beat)
+                if bstr.count(".") > 0:
+                    beat.duration = gp.Duration(int(bstr.rsplit(".")[-1]))
                 continue
             beat = gp.Beat(measure.voices, duration=curduration)
             measure.voices[0].beats.append(beat)
-            parts = token.split(".")
-            note = gp.Note(
-                beat,
-                value=int(parts[0]),
-                string=int(parts[1].split("{")[0]),
-            )
-            beat.notes.append(note)
-            if len(parts) == 3:
-                beat.duration = gp.Duration(int(parts[2]))
-            if "{" in parts[1]:
-                ef = parts[1].split("{")[1][:-1].strip("_").split("_")
-                i = 0
-                while i < len(ef):
-                    if ef[i] == "d":
-                        note.beat.duration.isDotted = True
-                    elif ef[i] == "v":
-                        note.effect.vibrato = True
-                    elif ef[i] == "h":
-                        note.effect.hammer = True
-                    elif ef[i] in ["b", "be"]:
-                        values = [int(v) for v in ef[i + 1].split("-")]
-                        if ef[i] == "b":
-                            stride = gp.BendEffect.maxPosition // (len(values) - 1)
-                            positions = [p * stride for p in range(len(values))]
-                            note.effect.bend = gp.BendEffect(
-                                points=[
-                                    gp.BendPoint(position=p, value=v)
-                                    for p, v in zip(positions, values)
-                                ]
-                            )
-                        else:
-                            note.effect.bend = gp.BendEffect(
-                                points=[
-                                    gp.BendPoint(
-                                        position=values[p * 2], value=values[p * 2 + 1]
-                                    )
-                                    for p in range(len(values) // 2)
-                                ],
-                            )
+            if "*" in bstr:
+                bstr, mult = bstr.split("*")
+                for _ in range(int(mult) - 1):
+                    measure.voices[0].beats.append(beat)
+            if (not bstr.startswith("(") and bstr.count(".") == 2) or bstr.count(
+                ")."
+            ) > 0:
+                body, dur = bstr.rsplit(".", 1)
+                beat.duration = gp.Duration(int(dur))
+            else:
+                body = bstr
+            body = body.strip(")").strip("(")
+            for nstr in body.split(";"):
+                fret, string_and_ef = nstr.split(".")
+                note = gp.Note(
+                    beat,
+                    value=int(fret),
+                    string=int(string_and_ef.split("{")[0]),
+                )
+                beat.notes.append(note)
+                if "{" in string_and_ef:
+                    ef = string_and_ef.split("{")[1][:-1].strip("_").split("_")
+                    i = 0
+                    while i < len(ef):
+                        if ef[i] == "d":
+                            note.beat.duration.isDotted = True
+                        elif ef[i] == "v":
+                            note.effect.vibrato = True
+                        elif ef[i] == "h":
+                            note.effect.hammer = True
+                        elif ef[i] in ["b", "be"]:
+                            values = [int(v) for v in ef[i + 1].split("-")]
+                            if ef[i] == "b":
+                                stride = gp.BendEffect.maxPosition // (len(values) - 1)
+                                positions = [p * stride for p in range(len(values))]
+                                note.effect.bend = gp.BendEffect(
+                                    points=[
+                                        gp.BendPoint(position=p, value=v)
+                                        for p, v in zip(positions, values)
+                                    ]
+                                )
+                            else:
+                                note.effect.bend = gp.BendEffect(
+                                    points=[
+                                        gp.BendPoint(
+                                            position=values[p * 2],
+                                            value=values[p * 2 + 1],
+                                        )
+                                        for p in range(len(values) // 2)
+                                    ],
+                                )
+                            i += 1
+                        elif ef[i] == "g":
+                            note.effect.ghostNote = (True,)
+                        elif ef[i] == "x":
+                            note.type = gp.NoteType.dead
+                        elif ef[i] == "r":
+                            note.type = gp.NoteType.rest
+                        elif ef[i] in ["-", "t"]:
+                            note.type = gp.NoteType.tie
+                        elif ef[i] == "lr":
+                            note.effect.letRing = True
+                        elif ef[i] == "st":
+                            note.effect.staccato = True
+                        elif ef[i] == "pm":
+                            note.effect.palmMute = True
+                        elif ef[i] == "nh":
+                            note.effect.harmonic = gp.NaturalHarmonic()
+                        elif ef[i] == "ah":
+                            note.effect.harmonic = gp.ArtificialHarmonic()
+                        elif ef[i] == "th":
+                            note.effect.harmonic = gp.TappedHarmonic()
+                        elif ef[i] == "ph":
+                            note.effect.harmonic = gp.PinchHarmonic()
+                        elif ef[i] == "sh":
+                            note.effect.harmonic = gp.SemiHarmonic()
+                        elif ef[i] == "tr":
+                            note.effect.trill = gp.TrillEffect()
+                        elif ef[i] == "f":
+                            note.beat.effect.fadeIn = True
+                        elif ef[i] == "tu":
+                            enters = int(ef[i + 1])
+                            times = dict(gp.Tuplet.supportedTuplets)[enters]
+                            note.beat.duration.tuplet = gp.Tuplet(enters, times)
+                            i += 1
                         i += 1
-                    elif ef[i] == "g":
-                        note.effect.ghostNote = (True,)
-                    elif ef[i] == "x":
-                        note.type = gp.NoteType.dead
-                    elif ef[i] == "r":
-                        note.type = gp.NoteType.rest
-                    elif ef[i] in ["-", "t"]:
-                        note.type = gp.NoteType.tie
-                    elif ef[i] == "lr":
-                        note.effect.letRing = True
-                    elif ef[i] == "st":
-                        note.effect.staccato = True
-                    elif ef[i] == "pm":
-                        note.effect.palmMute = True
-                    elif ef[i] == "nh":
-                        note.effect.harmonic = gp.NaturalHarmonic()
-                    elif ef[i] == "ah":
-                        note.effect.harmonic = gp.ArtificialHarmonic()
-                    elif ef[i] == "th":
-                        note.effect.harmonic = gp.TappedHarmonic()
-                    elif ef[i] == "ph":
-                        note.effect.harmonic = gp.PinchHarmonic()
-                    elif ef[i] == "sh":
-                        note.effect.harmonic = gp.SemiHarmonic()
-                    elif ef[i] == "tr":
-                        note.effect.trill = gp.TrillEffect()
-                    elif ef[i] == "f":
-                        note.beat.effect.fadeIn = True
-                    elif ef[i] == "tu":
-                        enters = int(ef[i + 1])
-                        times = dict(gp.Tuplet.supportedTuplets)[enters]
-                        note.beat.duration.tuplet = gp.Tuplet(enters, times)
-                        i += 1
-                    i += 1
         song.newMeasure()
     return song
 
 
-# song = gp.parse("test/metallica.gp4")
+# song = gp.parse("test/data/metallica.gp4")
 # bass = find_bass_track(song)
 # if not bass:
 #     raise ValueError("No bass track found")
 # else:
 #     print(f"Found bass track: {bass.name} (number {bass.number})")
 # print(track_to_alphatex(bass))
+
+
+# tex = """
+# \\title "Some chords"
+# .
+# (0.3{v h} 0.4{b (0 4)}).4 (3.3 3.4).4 (5.3 5.4).4 r.8 (0.3 0.4).8 |
+# r.8 (3.3 3.4).8 r.8 (6.3 6.4).8 (5.3 5.4).4 r.4 |
+# (0.3 0.4).4 (3.3 3.4).4 (5.3 5.4).4 r.8 (3.3 3.4).8 |
+# r.8 (0.3 0.4).8
+# """
+
+# # tex = """
+# # \\title "Repeated notes"
+# # .
+# # 3.3*4 | 4.3*4
+# # """
+
+# song = alphatex_to_song(tex)
+# tex2 = track_to_alphatex(song.tracks[0])
+# print("Before:")
+# print(tex)
+# print()
+# print("After:")
+# print(tex2)
+# song2 = alphatex_to_song(tex2)
+# tex3 = track_to_alphatex(song2.tracks[0])
+# print()
+# print("Second round:")
+# print(tex3)
--- test/test_conversion.py
+++ test/test_conversion.py
@@ -1,32 +1,58 @@
 from gp2tex import alphatex_to_song, track_to_alphatex
 
 
-def test_canonrock():
+def _test_conversion(tex):
     """
     Full round of conversion from .txt->.gp->.tex->.gp and compare
     """
-    tex = """
-    \\title "Canon Rock"
-    \\subtitle "JerryC"
-    \\tempo 90
-    .
-    :2 19.2{v f} 17.2{v f} |
-    15.2{v f} 14.2{v f} |
-    12.2{v f} 10.2{v f} |
-    12.2{v f} 14.2{v f}.4 :8 15.2 17.2 |
-    14.1.2 :8 17.2 15.1 14.1{h} 17.2 |
-    15.2{v d}.4 :16 17.2{h} 15.2 :8 14.2 14.1 17.1{b (0 4 4 0)}.4 |
-    15.1.8 :16 14.1{tu 3} 15.1{tu 3} 14.1{tu 3} :8 17.2 15.1 14.1 :16 12.1{tu 3} 14.1{tu 3} 12.1{tu 3} :8 15.2 14.2 |
-    12.2 14.3 12.3 15.2 :32 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h}"""
-
     song = alphatex_to_song(tex)
-    # gp.write(song, "test/results/canon_rock.gp")
-
     tex2 = track_to_alphatex(song.tracks[0])
-
     song2 = alphatex_to_song(tex2)
-    # gp.write(song2, "test/results/canon_rock2.gp")
 
     assert len(song2.measureHeaders) == len(song.measureHeaders)
     assert len(song2.tracks[0].measures) == len(song.tracks[0].measures)
     assert song2.tracks[0] == song.tracks[0]
+
+    tex3 = track_to_alphatex(song2.tracks[0])
+    assert tex2 == tex3
+
+
+def test_canonrock():
+    _test_conversion(
+        """
+\\title "Canon Rock"
+\\subtitle "JerryC"
+\\tempo 90
+.
+:2 19.2{v f} 17.2{v f} |
+15.2{v f} 14.2{v f} |
+12.2{v f} 10.2{v f} |
+12.2{v f} 14.2{v f}.4 :8 15.2 17.2 |
+14.1.2 :8 17.2 15.1 14.1{h} 17.2 |
+15.2{v d}.4 :16 17.2{h} 15.2 :8 14.2 14.1 17.1{b (0 4 4 0)}.4 |
+15.1.8 :16 14.1{tu 3} 15.1{tu 3} 14.1{tu 3} :8 17.2 15.1 14.1 :16 12.1{tu 3} 14.1{tu 3} 12.1{tu 3} :8 15.2 14.2 |
+12.2 14.3 12.3 15.2 :32 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h} 14.2{h} 15.2{h}"""
+    )
+
+
+def test_chords():
+    _test_conversion(
+        """
+\\title "Some chords"
+.
+(0.3{v h} 0.4{b (0 4)}).4 (3.3 3.4).4 (5.3 5.4).4 r.8 (0.3 0.4).8 |
+r.8 (3.3 3.4).8 r.8 (6.3 6.4).8 (5.3 5.4).4 r.4 |
+(0.3 0.4).4 (3.3 3.4).4 (5.3 5.4).4 r.8 (3.3 3.4).8 |
+r.8 (0.3 0.4).8
+"""
+    )
+
+
+def test_repeated():
+    _test_conversion(
+        """
+\\title "Repeated notes"
+.
+3.3*4 | 4.3*4
+"""
+    )
