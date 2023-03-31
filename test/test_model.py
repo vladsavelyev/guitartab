cdf304b48575e872a7554e3c5947122dbf649669
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2023-03-31 13:44:15 +1100

    Fix the overfitting test

--- test/test_model.py
+++ test/test_model.py
@@ -2,75 +2,92 @@ from transformers import (
     Trainer,
     TrainingArguments,
     DataCollatorForLanguageModeling,
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    GenerationConfig,
 )
-
-import os
-
-print("CWD", os.getcwd())
 
 from datasets import Dataset
-from guitartab import model, tokenizer, dataset, generation_config
 
 
-# def test_overfit(tmp_path):
-tmp_path = "tmp"
-"""
-When running repeatedly on the same batch, model is expected to
-overfit to 100% accuracy. if that doesn't happen, it means the model
-is not learning anything
-"""
-example = {
-    "tex": """
-\\title "My Song"
-\\tempo 90
-.
-\\track
-\\instrument 42
-1.1 2.1 3.1 4.1 |
-\\track
-\\tuning A1 D2 A2 D3 G3 B3 E4
-4.1 3.1 2.1 1.1 |
-"""
-}
-dataset = Dataset.from_list([example])
-
-trainer = Trainer(
-    model=model,
-    data_collator=DataCollatorForLanguageModeling(
-        tokenizer,
-        mlm=False,
-    ),
-    train_dataset=dataset,
-    eval_dataset=dataset,
-    args=TrainingArguments(
-        output_dir=tmp_path,
-        report_to=[],
-        max_steps=20,
-        per_device_train_batch_size=1,
-        per_device_eval_batch_size=1,
-    ),
-)
-# taking single batch
-batch = next(iter(trainer.get_train_dataloader()))
-batch = {k: v.to(trainer.args.device) for k, v in batch.items()}
-trainer.create_optimizer()
-for _ in range(20):
-    outputs = trainer.model(**batch)
-    loss = outputs.loss
-    loss.backward()
-    trainer.optimizer.step()
-    trainer.optimizer.zero_grad()
-
-generated_tex = model.generate([model.eos_token], generation_config=generation_config)
-# generated_tex = generator([tokenizer.bos_token])[0][0]["generated_text"]
-training_tex = tokenizer.decode(batch["input_ids"][0].tolist())
-print("Training tex:")
-print(training_tex)
-print()
-print("Generated:")
-print(generated_tex)
+MODEL = "vldsavelyev/guitar_tab_gpt2"
 
 
-# if __name__ == "__main__":
-#     import os
-#     test_overfit("tmp")
+def test_overfit(tmp_path):
+    """
+    When running repeatedly on the same batch, model is expected to
+    overfit to 100% accuracy. if that doesn't happen, it means the model
+    is not learning anything
+    """
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    model = AutoModelForCausalLM.from_pretrained(MODEL)
+    example = {
+        "tex": """
+    \\title "Song"
+    .
+    1.1 2.1 3.1 4.1 |
+    """
+    }
+    dataset = Dataset.from_list([example])
+    dataset = dataset.map(
+        lambda b: {
+            "tex": [f"{tokenizer.bos_token}{x}{tokenizer.eos_token}" for x in b["tex"]]
+        },
+        batched=True,
+        remove_columns=dataset.column_names,
+    )
+    dataset = dataset.map(
+        lambda b: tokenizer(
+            b["tex"],
+            max_length=model.config.n_ctx,
+            truncation=True,
+            return_overflowing_tokens=True,
+            stride=int(model.config.n_ctx * 0.2),
+        ),
+        batched=True,
+        remove_columns=dataset.column_names,
+    ).select_columns("input_ids")
+
+    trainer = Trainer(
+        model=model,
+        data_collator=DataCollatorForLanguageModeling(
+            tokenizer,
+            mlm=False,
+        ),
+        train_dataset=dataset,
+        eval_dataset=dataset,
+        args=TrainingArguments(
+            output_dir=tmp_path,
+            report_to=[],
+            max_steps=20,
+            per_device_train_batch_size=1,
+            per_device_eval_batch_size=1,
+        ),
+    )
+    # taking single batch
+    batch = next(iter(trainer.get_train_dataloader()))
+    batch = {k: v.to(trainer.args.device) for k, v in batch.items()}
+    trainer.create_optimizer()
+    for _ in range(40):
+        outputs = trainer.model(**batch)
+        loss = outputs.loss
+        loss.backward()
+        trainer.optimizer.step()
+        trainer.optimizer.zero_grad()
+
+    generation_config = GenerationConfig.from_pretrained(
+        MODEL, "generation_config.json"
+    )
+    training_tokens = batch["input_ids"][0]
+    training_tex = tokenizer.decode(training_tokens.tolist())
+    generated_tokens = model.generate(
+        generation_config=generation_config,
+        max_length=len(training_tokens),
+    )[0]
+    generated_tex = tokenizer.decode(generated_tokens.tolist())
+    print("Training tex:")
+    print(training_tex)
+    print()
+    print("Generated:")
+    print(generated_tex)
+    assert training_tex == generated_tex
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# %% MODEL AND TOKENIZER
+# %% IMPORTS
 
 import os
 import math
@@ -6,21 +6,22 @@ from pathlib import Path
 
 import datasets, transformers, evaluate
 from transformers import (
+    GPT2LMHeadModel,
+    AutoConfig,
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    GenerationConfig,
     Trainer,
     TrainingArguments,
     TrainerCallback,
     DataCollatorForLanguageModeling,
     EvalPrediction,
     pipeline,
+    trainer_utils,
 )
-from transformers.trainer_utils import get_last_checkpoint
 import coloredlogs
-
 import guitarpro as gp
-
 from gp2tex import alphatex_to_song
-from guitartab.model import model, tokenizer, dataset, generation_config, MODEL
-
 
 coloredlogs.install(level="info")
 datasets.logging.set_verbosity_info()
@@ -33,12 +34,96 @@ push_to_hub = True
 
 token = os.getenv("HUB_TOKEN")
 if push_to_hub and not token:
-    raise ValueError(
-        "push_to_hub is set to True, but HUB_TOKEN environment variable is not set"
+    push_to_hub = False
+    print("Cannot push to hub without HUB_TOKEN")
+
+MODEL = "vldsavelyev/guitar_tab_gpt2"
+DATASET = "vldsavelyev/guitar_tab"
+BASE_MODEL = "gpt2"
+FROM_SCRATCH = False
+
+# %% DATASET
+
+print(f"Loading dataset from remote repo {DATASET}")
+dataset = datasets.load_dataset(DATASET)
+
+# %% TOKENIZER
+
+if FROM_SCRATCH:
+    base_tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
+    pad_token = "<|pad|>"
+    tokenizer = base_tokenizer.train_new_from_iterator(
+        (e["tex"] for e in dataset["train"]),
+        vocab_size=500,
+        new_special_tokens=[pad_token],
+    )
+    tokenizer.pad_token = "<|pad|>"
+    tokenizer.push_to_hub(MODEL)
+else:
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+
+dataset = dataset["train"].train_test_split(test_size=10)
+
+# Wrap novel chapters with BOS and EOS tokens (tokenizer wouldn't
+# do that even if add_special_tokens is True, see
+# https://github.com/huggingface/transformers/issues/3311)
+dataset = dataset.map(
+    lambda b: {
+        "tex": [f"{tokenizer.bos_token}{x}{tokenizer.eos_token}" for x in b["tex"]]
+    },
+    batched=True,
+    remove_columns=dataset["train"].column_names,
+)
+
+# %% MODEL
+if FROM_SCRATCH:
+    print(f"Initializing model {MODEL}")
+    config = AutoConfig.from_pretrained(
+        BASE_MODEL,
+        vocab_size=len(tokenizer),
+        eos_token_id=tokenizer.eos_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        pad_token_id=tokenizer.pad_token_id,
+        n_embd=96,  # smaller vocab -> smaller embedding
+        n_layer=8,
+        n_head=8,
+    )
+    model = GPT2LMHeadModel(config)
+    print(f"Model parameters: {model.num_parameters():,}")
+    model.push_to_hub(MODEL)
+
+    generation_config = GenerationConfig(
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.pad_token_id,
+        top_k=50,
+        top_p=0.95,
+        do_sample=True,
+        num_return_sequences=1,
+        max_length=200,
+    )
+    generation_config.push_to_hub(MODEL, "generation_config.json")
+else:
+    model = AutoModelForCausalLM.from_pretrained(MODEL)
+    generation_config = GenerationConfig.from_pretrained(
+        MODEL, "generation_config.json"
     )
 
-# %% SETUP TRAINER
 
+# %% PREP DATASET
+dataset = dataset.map(
+    lambda b: tokenizer(
+        b['tex'],
+        max_length=model.config.n_ctx,
+        truncation=True,  # because of the option below, it will chunk
+        return_overflowing_tokens=True,  # ...tokens, not trancate
+        # we want the chunks to overlap by 20%
+        stride=int(model.config.n_ctx * 0.2),
+    ),
+    batched=True,
+    remove_columns=dataset['train'].column_names
+).select_columns('input_ids')
+
+# %% SETUP TRAINER
 repos_dir = Path(os.getenv("HUB_REPOS") or "").resolve()
 save_dir = repos_dir / "models" / MODEL
 
@@ -83,7 +168,6 @@ else:
         per_device_train_batch_size=1,
         per_device_eval_batch_size=1,
     )
-
 
 generator = pipeline(
     "text-generation",
@@ -144,6 +228,6 @@ trainer = Trainer(
 # %% TRAIN
 if not dry_run:
     trainer.evaluate()  # to early test evaluation works
-    trainer.train(resume_from_checkpoint=get_last_checkpoint(save_dir))
+    trainer.train(resume_from_checkpoint=trainer_utils.get_last_checkpoint(save_dir))
     if push_to_hub:
         trainer.save_model()  # also calls push_to_hub
